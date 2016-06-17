e79a59b9f082d7e429276ceb4817f907ed700bd3
Venkatraman Srikanth <venkatramansrikanth@outlook.com>
2016-06-18 04:41:23 +0530

    First commit

---
+++ clean_mentors.py
@@ -0,0 +1,26 @@
+import csv,json,sys
+def mentorCleaner(fileName):
+    try:
+        mentorCsv = csv.reader(open(fileName, 'rb'))
+        mentors = list(mentorCsv)
+        dictMentors = []
+        for row in mentors:
+            tempDict={}
+            tempDict['name'] = row[1]
+            tempDict['rollNo'] = row[2]
+            tempDict['gender'] = row[3]
+            tempDict['email'] = row[4]
+            tempDict['dept'] = row[5]
+            tempDict['gpa'] = float(row[6])
+            tempDict['dasa'] = row[7]
+            tempDict['language'] = row[8]
+            tempDict['state'] = row[9]
+            dictMentors.append(tempDict)
+        with open('mentors_raw_data.json', 'w') as outfile:
+            json.dump(dictMentors, outfile)
+    except IOError:
+        print("File could not be found.")
+        sys.exit()
+if __name__=="__main__":    
+    mentorFileName = "mentors_raw_data.csv"
+    mentorCleaner(mentorFileName)
