8fbf3f168429aee0028834d63a6d732e87161b5c
Venkatraman Srikanth <venkatramansrikanth@outlook.com>
2016-06-18 20:59:17 +0530

    Added server and updated values

---
+++ server.py
@@ -0,0 +1,11 @@
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 7200
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "Listening on port", PORT
+httpd.serve_forever()
